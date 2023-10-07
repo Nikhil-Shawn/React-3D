c6079ec7952695b9d2d9b8d6a910f8ea5e999cba
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2023-10-07 22:55:36 +0530

    Creation

--- src/constants/index.js
+++ src/constants/index.js
@@ -165,6 +165,7 @@ import {
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
+        "Automated generation of information for client which resulted in saving 30% of clients weekly time"
       ],
     }
   ];
